2f2862ec8287557fef64ca0fd20c3f1259241ea6
kaleenderoff <kaleenderoff@gmail.com>
2022-12-17 06:43:05 -0500

    proyecto completado

---
+++ controllers/client-controller.js
@@ -0,0 +1,46 @@
+import { clientServices } from "../service/cliente-service.js";
+
+const crearNuevaLinea = (nombre,email, id) => {
+    const linea = document.createElement('tr');
+    const contenido = `
+        <td class="td" data-td>${nombre}</td>
+        <td>${email}</td>
+        <td>
+            <ul class="table__button-control">
+                <li>
+                    <a
+                    href="../screens/editar_cliente.html?id=${id}"
+                    class="simple-button simple-button--edit"
+                    >Editar</a
+                    >
+                </li>
+                <li>
+                    <button
+                    class="simple-button simple-button--delete"
+                    type="button"
+                    id="${id}"
+                    >
+                    Eliminar
+                    </button>
+                </li>
+            </ul>
+        </td>
+    `;
+    linea.innerHTML = contenido;
+    const btn = linea.querySelector('button');
+    btn.addEventListener('click', () => {
+        const id = btn.id;
+        clientServices.eliminarCliente(id).then(respuesta => {
+            console.log(respuesta);
+        }).catch(error => alert('An error has ocurried'));
+    });
+    return linea;
+};
+
+const table = document.querySelector('[data-table]');
+
+clientServices.listaClientes().then((data) => {
+    data.forEach( ({nombre, email, id}) => {
+        table.appendChild(crearNuevaLinea(nombre, email, id));
+    });
+}).catch((error) => alert('An error has ocurried'));
